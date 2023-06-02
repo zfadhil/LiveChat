fcbb627df97823dbf16aaa5760f63477964a7e45
FadhilYTProgrammer <fadhil18022007@gmail.com>
2023-06-02 10:30:37 +0700

    chatbox message design

--- resources/views/livewire/chat/chatbox.blade.php
+++ resources/views/livewire/chat/chatbox.blade.php
@@ -22,8 +22,25 @@
     </div>
 
     <div class="chatbox_body">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro id, eum repellat maiores odit pariatur magnam
-        hic culpa voluptatibus totam explicabo eligendi laborum adipisci beatae veniam. Aliquam assumenda iusto
-        dignissimos.
+        <div class="msg_body msg_body_receiver">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti reiciendis beatae asperiores sit suscipit
+            ratione pariatur vero et non. Expedita accusamus quia vel optio delectus facere iusto accusantium fugit rem!
+            <div class="msg_body_footer">
+                <div class="date">2 days ago</div>
+                <div class="read">
+                    <i class="bi bi-check"></i>
+                </div>
+            </div>
+        </div>
+        <div class="msg_body msg_body_me">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti reiciendis beatae asperiores sit suscipit
+            ratione pariatur vero et non. Expedita accusamus quia vel optio delectus facere iusto accusantium fugit rem!
+            <div class="msg_body_footer">
+                <div class="date">2 days ago</div>
+                <div class="read">
+                    <i class="bi bi-check"></i>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
