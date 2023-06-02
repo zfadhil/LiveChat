8031737e221023fcbb1741ba49adf63b63fd44b7
FadhilYTProgrammer <fadhil18022007@gmail.com>
2023-06-02 10:38:47 +0700

    send message design

--- resources/views/livewire/chat/send-message.blade.php
+++ resources/views/livewire/chat/send-message.blade.php
@@ -1,4 +1,10 @@
 <div>
     {{-- The Master doesn't talk, he acts. --}}
-    <div class="chatbox_footer"></div>
+    <div class="chatbox_footer">
+
+        <div class="custom_form_group">
+            <input type="text" class="control" placeholder="write message">
+            <button class="submit" type="submit">Send</button>
+        </div>
+    </div>
 </div>
