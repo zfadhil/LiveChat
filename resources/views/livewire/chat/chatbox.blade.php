8a7cf6f38ac5af5660f12db08d9396ed0fce4cd6
FadhilYTProgrammer <fadhil18022007@gmail.com>
2023-06-02 09:41:59 +0700

    design chatbox header

--- resources/views/livewire/chat/chatbox.blade.php
+++ resources/views/livewire/chat/chatbox.blade.php
@@ -1,10 +1,29 @@
 <div>
     {{-- In work, do what you enjoy. --}}
     <div class="chatbox_header">
-        header
+        <div class="return">
+            <i class="bi bi-arrow-left"></i>
+        </div>
+        <div class="img_container">
+            <img src="https://picsum.photos/id/237/200/300" alt="">
+        </div>
+        <div class="name">Ziry</div>
+        <div class="info">
+            <div class="info_item">
+                <i class="bi bi-telephone-fill"></i>
+            </div>
+            <div class="info_item">
+                <i class="bi bi-image"></i>
+            </div>
+            <div class="info_item">
+                <i class="bi bi-info-circle"></i>
+            </div>
+        </div>
     </div>
 
     <div class="chatbox_body">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro id, eum repellat maiores odit pariatur magnam hic culpa voluptatibus totam explicabo eligendi laborum adipisci beatae veniam. Aliquam assumenda iusto dignissimos.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro id, eum repellat maiores odit pariatur magnam
+        hic culpa voluptatibus totam explicabo eligendi laborum adipisci beatae veniam. Aliquam assumenda iusto
+        dignissimos.
     </div>
 </div>
