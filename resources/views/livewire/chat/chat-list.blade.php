990aed27ca6e9a8b2bc29cb48ba85a185f0bf261
FadhilYTProgrammer <fadhil18022007@gmail.com>
2023-06-06 14:44:52 +0700

    show all convesations

--- resources/views/livewire/chat/chat-list.blade.php
+++ resources/views/livewire/chat/chat-list.blade.php
@@ -9,23 +9,20 @@
         </div>
     </div>
 
+    @if (count($conversations) > 0)
+    @foreach ($conversations as $conversation)
     <div class="chatlist_body">
         <div class="chatlist_item">
             <div class="chatlist_img_container">
-                <img src="https://picsum.photos/id/237/200/300" alt="">
+                <img src="https://picsum.photos/id/{{ $this->getChatUserInstance($conversation, $name = 'id') }}/200/300" alt="">
             </div>
             <div class="chatlist_info">
                 <div class="top_row">
-                    <div class="list_username">Zirjy</div>
-                    <span class="date">2d</span>
+                    <div class="list_username">{{ $this->getChatUserInstance($conversation, $name = 'name') }}</div>
+                    <span class="date">{{ $conversation->messages->last()->created_at->shortAbsoluteDiffForHumans() }}</span>
                 </div>
                 <div class="bottom_row">
-                    <div class="message_body text-truncate">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                        Unde ratione optio, fugit sequi ea temporibus nobis laudantium 
-                        consequatur saepe magni dolores accusamus voluptate, exercitationem 
-                        consectetur at rem facere! Quasi, at.
-                    </div>
+                    <div class="message_body text-truncate">{{ $conversation->messages->last()->body }}</div>
                     <div class="unread_count">
                         100
                     </div>
@@ -33,4 +30,10 @@
             </div>
         </div>
     </div>
+    @endforeach
+    @else
+        You Have no Conversation
+    @endif
+
+
 </div>
